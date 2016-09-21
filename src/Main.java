a9b157fd9150a6cbf8a84a32218fbe52e8aa24ab
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-09-20 20:54:24 -0400

    Max substring array length

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
-public class Main {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main {
+    public static void main(String args[]) throws IOException {
+       String string = "bcdealmnop";
+        char[] charArray=string.toCharArray();
+
+        int maxString = MaxString(charArray,0,charArray.length-1);
+
+        System.out.println(maxString);
+    }
+
+    private static int MaxString(char[] charArray, int l, int r) {
+
+        if(l==r){
+            return 1;
+        }
+
+        int m = (l+r)/2;
+
+        return max(MaxString(charArray, l, m),
+                MaxString(charArray, m+1, r),
+                MaxCrossingString(charArray, l, m, r));
+    }
+
+    // A utility funtion to find maximum of two integers
+    static int max(int a, int b) { return (a > b)? a : b; }
+
+    // A utility funtion to find maximum of three integers
+     static int max(int a, int b, int c) { return max(max(a, b), c); }
+
+    private static int MaxCrossingString(char[] charArray, int l, int m, int r) {
+
+        int length = 1;
+        int maxSubLength = Integer.MIN_VALUE;
+
+        for (int i = l;i<r;i++)
+        {
+            if(charArray[i]<charArray[i+1])
+            {
+                length++;
+                if (length > maxSubLength)
+                    maxSubLength = length;
+            }
+            else
+                return 0;
+        }
+        return maxSubLength;
     }
 }
